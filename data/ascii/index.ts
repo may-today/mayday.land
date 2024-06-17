196997bd092b7bc5a87570ba8bb963c5cf513741
ddiu8081 <ddiu8081@gmail.com>
2024-06-17 17:08:14 +0800

    chore: ascii code

--- data/ascii/index.ts
+++ data/ascii/index.ts
@@ -1,5 +1,9 @@
 import toulou from './toulou'
+import diyizhangchuangzuozhuanji from './diyizhangchuangzuozhuanji'
+import zizhuan from './zizhuan'
 
 export const list = {
   toulou,
+  diyizhangchuangzuozhuanji,
+  zizhuan,
 }
