b36ed3466aa08358f45f4a980d97ced6bbd1ec3f
ddiu8081 <ddiu8081@gmail.com>
2024-06-23 14:35:42 +0800

    fix: can't click link in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -36,16 +36,16 @@ const Footer: Component = () => {
           onClick={toggleFooter}
           onKeyPress={(e) => e.key === 'Enter' && toggleFooter}
         >
-          <div class="absolute bottom-10 right-2 flex flex-col space-y-2 px-4 py-2 border border-zinc-800 bg-zinc-950 rounded-md text-xs z-30">
-            <h1>MaydayLand·「透露」聊天室</h1>
-            <div>
-              <p>制作: <a class="text-sky-200" target="_blank" rel="noreferrer" href="https://ddiu.io">Diu</a></p>
-              <p>源代码: <a class="text-sky-200" target="_blank" rel="noreferrer" href="https://github.com/may-today/mayday.land">may-today/mayday.land</a></p>
-            </div>
-            <div>
-              <p>部分ascii字符画来自: ringomango</p>
-              <p>字体: Cubic-11</p>
-            </div>
+        </div>
+        <div class="absolute bottom-10 right-2 flex flex-col space-y-2 px-4 py-2 border border-zinc-800 bg-zinc-950 rounded-md text-xs z-30">
+          <h1>MaydayLand·「透露」聊天室</h1>
+          <div>
+            <p>制作: <a class="text-sky-200" target="_blank" rel="noreferrer" href="https://ddiu.io">Diu</a></p>
+            <p>源代码: <a class="text-sky-200" target="_blank" rel="noreferrer" href="https://github.com/may-today/mayday.land">may-today/mayday.land</a></p>
+          </div>
+          <div>
+            <p>部分ascii字符画来自: ringomango</p>
+            <p>字体: Cubic-11</p>
           </div>
         </div>
       </Show>
