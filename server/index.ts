5c2c8cd2bf7acb04899559e2149412bf7a5c128f
ddiu8081 <ddiu8081@gmail.com>
2024-06-23 18:16:18 +0800

    feat: add tms check

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,17 @@
 import type { User, Message } from '~/types'
+import { tmsCheck } from './tms'
 
 type UserData = {
   user: User
 }
 
 let userCount = 0
+
+const genTime = () => {
+  const date = new Date()
+  const dd = [date.getHours(), date.getMinutes(), date.getSeconds()].map((n) => n.toString().padStart(2, '0'))
+  return dd.join(':')
+}
 
 const server = Bun.serve<UserData>({
   fetch(req, server) {
@@ -48,11 +55,20 @@ const server = Bun.serve<UserData>({
         ws.send(JSON.stringify({ user: 'server_ping', message: userCount }))
         return
       }
+      if (typeof message !== 'string') {
+        return
+      }
       const _message = {
         user: ws.data.user,
         message,
       } as Message
-      console.log('[ws]', ws.data.user?.name, message)
+      console.log(`[ws] ${genTime()} ${ws.data.user?.name}#${ws.data.user?.suffix}: ${message.length}`)
+      if (message.length > 5) {
+        const pass = await tmsCheck(message)
+        if (!pass) {
+          _message.message = '***'
+        }
+      }
       server.publish('chat', JSON.stringify(_message))
     },
     close(ws, event) {
---
+++ server/tms.ts
@@ -0,0 +1,92 @@
+import crypto from 'node:crypto'
+
+const ENDPOINT = Bun.env.TMS_ENDPOINT || ''
+const SECRET_ID = Bun.env.TMS_SECRET_ID || ''
+const SECRET_KEY = Bun.env.TMS_SECRET_KEY || ''
+
+const _sortParams = (params: Record<string, string | number>) => {
+  let strParam = ''
+  const keys = Object.keys(params)
+  keys.sort()
+  for (const k in keys) {
+    strParam += `&${keys[k]}=${params[keys[k]]}`
+  }
+  return strParam.slice(1)
+}
+
+const _sha1 = (secretKey: string, strsign: string) => {
+  const signMethodMap = { HmacSHA1: 'sha1' }
+  const hmac = crypto.createHmac(signMethodMap.HmacSHA1, secretKey || '')
+  return hmac.update(Buffer.from(strsign, 'utf8')).digest('base64')
+}
+
+const _genSignature = (params: Record<string, string | number>) => {
+  const method = 'POST'
+  const path = '/'
+  const strParam = _sortParams(params)
+  const strSign = `${method}${ENDPOINT}${path}?${strParam}`
+  return _sha1(SECRET_KEY, strSign)
+}
+
+const _utf8ToB64 = (str: string) => {
+  return btoa(
+    encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, (match, p1) => {
+      // @ts-expect-error
+      return String.fromCharCode(`0x${p1}`)
+    })
+  )
+}
+
+export const tmsCheck = async (content: string) => {
+  const Region = 'ap-shanghai'
+  const Action = 'TextModeration'
+  const Version = '2020-12-29'
+  const Timestamp = Math.round(Date.now() / 1000)
+  const Nonce = Math.round(Math.random() * 65535)
+  const base64Content = _utf8ToB64(content)
+
+  const params = {
+    Action,
+    Region,
+    Timestamp,
+    Nonce,
+    SecretId: SECRET_ID,
+    Version,
+    Content: base64Content,
+  } as Record<string, string | number>
+
+  const signature = _genSignature(params)
+  params.Signature = signature
+
+  const response = await fetch(`https://${ENDPOINT}/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+    },
+    body: Object.keys(params)
+      .map(
+        (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+      )
+      .join('&'),
+  })
+  const data = await response.json()
+
+  if (!response.ok || !data.Response || !data.Response.Suggestion) {
+    console.error('[TMS] check Error:', content)
+    return true
+  }
+  if (data.Response.Suggestion === 'Pass') {
+    return true
+  }
+  if (data.Response.Suggestion === 'Review') {
+    const label = data.Response.Label
+    console.warn(`[TMS] check Review (${label})`, content)
+    return true
+  }
+  if (data.Response.Suggestion === 'Block') {
+    const label = data.Response.Label
+    console.error(`[TMS] check Block (${label})`, content)
+    return false
+  }
+  return true
+}
