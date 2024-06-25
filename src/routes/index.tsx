d4f068b3690292915c66d078d4c7298ba9439de8
ddiu8081 <ddiu8081@gmail.com>
2024-06-25 21:21:44 +0800

    chore: remove ws server

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -26,14 +26,14 @@ const countApi = 'https://maydayland-server-tn.ddiu.site/count'
 const wsServerPrefix = 'wss://maydayland-server-tn.ddiu.site/ws'
 const maxMessageHistoryLength = 6
 
-const getInitialCount = async () => {
-  const response = await fetch(countApi)
-  return Number(await response.text())
-}
+// const getInitialCount = async () => {
+//   const response = await fetch(countApi)
+//   return Number(await response.text())
+// }
 
 export default function Page() {
   let scrollRef: HTMLDivElement
-  const [initialCount] = createResource(getInitialCount)
+  // const [initialCount] = createResource(getInitialCount)
   const [messages, setMessages] = createStore<Message[]>([])
   const [user, setUser] = createSignal<User | null>(null)
   const [currentAlertMessage, setCurrentAlertMessage] = createSignal('')
@@ -126,16 +126,16 @@ export default function Page() {
     (isScrollToBottom() || force) && element.scrollTo({ top: element.scrollHeight })
   }, 300)
 
-  const currentOnline = () => {
-    if (onlineCount() >= 0) {
-      return onlineCount()
-    }
-    return initialCount() || 0
-  }
+  // const currentOnline = () => {
+  //   if (onlineCount() >= 0) {
+  //     return onlineCount()
+  //   }
+  //   return initialCount() || 0
+  // }
 
   const onUserChange = (newUser: User) => {
     setUser(newUser)
-    connectWs(newUser)
+    // connectWs(newUser)
     playRemainingAudio();
   }
 
@@ -194,9 +194,9 @@ export default function Page() {
     <main class="flex flex-col w-screen h-[100svh] bg-black whitespace-pre-wrap overflow-hidden">
       <Title>MaydayLand·「透露」聊天室</Title>
       <Header>
-        <span>（目前共有</span>
+        {/* <span>（目前共有</span>
         <span class="ansi-cyan">{currentOnline()}</span>
-        <span>人上线）</span>
+        <span>人上线）</span> */}
       </Header>
       <div class="relative flex-1 overflow-y-scroll overflow-x-hidden pb-4" ref={scrollRef!}>
         <Login onSubmit={onUserChange} />
@@ -222,7 +222,7 @@ export default function Page() {
           </Show>
         </Show>
       </div>
-      <Show when={!!user() && anim2Finished()}>
+      {/* <Show when={!!user() && anim2Finished()}>
         <Show when={!isScrollToBottom()}>
           <ScrollToBottom
             onClickScrollTop={onClickScrollTop}
@@ -233,7 +233,7 @@ export default function Page() {
         <Show when={currentAlertMessage()}>
           <Alert message={currentAlertMessage()} />
         </Show>
-      </Show>
+      </Show> */}
       <Footer/>
     </main>
   )
